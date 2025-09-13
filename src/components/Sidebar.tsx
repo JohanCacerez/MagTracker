3f7de220974baca79ebefd7371960fc04e246991
JohanCacerez <johancacerez10@gmail.com>
2025-09-12 20:10:18 -0600

    tailwind config

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,35 +5,35 @@ import { HiOutlineChartBar, HiOutlineUserCircle } from "react-icons/hi";
 
 const Sidebar: React.FC = () => {
   return (
-    <div className="w-60 h-screen bg-gray-800 text-white flex flex-col p-4">
+    <div className="w-60 h-screen bg-primary text-white flex flex-col p-4">
       {/* Parte superior: Usuario + Opciones */}
       <div>
         {/* Usuario */}
         <div className="flex items-center justify-between mb-6">
-          <div className="flex items-center gap-2">
+          <div className="flex items-center text-text-dark gap-2">
             <HiOutlineUserCircle size={35} />
-            <span className="font-semibold">Nombre Usuario</span>
+            <span className="font-semibold text-text-dark">Nombre Usuario</span>
           </div>
-          <button className="bg-gray-700 p-2 rounded-full hover:bg-gray-600 cursor-pointer">
+          <button className="bg-primary-dark p-2 rounded-full text-text-dark hover:bg-primary-dark cursor-pointer">
             <FaCog size={18} />
           </button>
         </div>
 
         {/* Opciones */}
         <div className="flex flex-col space-y-1">
-          <button className="flex items-center gap-3 px-4 py-2 hover:bg-gray-700 rounded cursor-pointer">
+          <button className="flex items-center gap-3 px-4 py-2 font-semibold hover:bg-primary-dark rounded text-text-dark cursor-pointer">
             <HiOutlineChartBar />
             <span>Dashboard</span>
           </button>
           <hr className="border-gray-700" />
 
-          <button className="flex items-center gap-3 px-4 py-2 hover:bg-gray-700 rounded cursor-pointer">
+          <button className="flex items-center gap-3 px-4 py-2 hover:bg-primary-dark rounded text-text-dark cursor-pointer">
             <BiBuilding />
             <span>Magazines</span>
           </button>
           <hr className="border-gray-700" />
 
-          <button className="flex items-center gap-3 px-4 py-2 hover:bg-gray-700 rounded cursor-pointer">
+          <button className="flex items-center gap-3 px-4 py-2 hover:bg-primary-dark rounded text-text-dark cursor-pointer">
             <BiCar />
             <span>Dollies</span>
           </button>
@@ -43,7 +43,7 @@ const Sidebar: React.FC = () => {
       {/* Parte inferior: Logout */}
       <div className="mt-auto">
         <hr className="border-gray-700 mb-2" />
-        <button className="flex items-center gap-3 w-full px-4 py-2 hover:bg-gray-700 rounded cursor-pointer">
+        <button className="flex items-center gap-3 w-full px-4 py-2 hover:bg-primary-dark rounded text-text-dark cursor-pointer">
           <FaSignOutAlt />
           <span>Cerrar Sesión</span>
         </button>
